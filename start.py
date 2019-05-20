f5d30b092e6d423667db519bfeda0f38b5303ae2
jianzhi.lu <jianzhi.lu@beibei.com>
2019-05-20 08:12:10 +0800

    函数相关

--- start.py
+++ start.py
@@ -1,3 +1,8 @@
+import function_py as t1  # 导包并且别名
+import sys
+sys.path[0] = '/home/ian/development/python/project/python_learn/sub' # 为了找到另外文件夹下的模块,修改路径
+import sub_function_py as t2  # 导其他文件夹下的包并且别名
+
 # 第一个python项目
 p = 0
 p = '11s'
@@ -54,3 +59,19 @@ else:
 
 print(id(numbers))
 print(id(e))
+
+
+def find_2():
+    array = [1, 2, 3, 4, 5]
+    for i in array:
+        if (i == 2):
+            print('有2')
+            break
+
+
+find_2()
+
+array = [1, 2, 3, 4, 5]
+
+print(t1.find_number(array, "5"))  # 使用导进来的包调用函数
+print(t2.find_number(array, "5"))  # 使用导进来的包调用函数
