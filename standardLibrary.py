dcb710196e737c02b4dd776d15a12a7acf34e36b
jianzhi.lu <jianzhi.lu@beibei.com>
2019-05-25 13:05:52 +0800

    sys模块

--- standardLibrary.py
+++ standardLibrary.py
@@ -1,5 +1,8 @@
-from datetime import datetime, date, time
+from datetime import datetime, date, time as datetime_time
 import math
+import os
+import sys
+import time
 
 # dattetime
 today = datetime.now()
@@ -12,7 +15,7 @@ print(datetime.timestamp(today))
 print(datetime.fromtimestamp(datetime.timestamp(today)))
 
 date1 = date(2019, 7, 14)
-time1 = time(14, 14, 14)
+time1 = datetime_time(14, 14, 14)
 print(datetime.combine(date1, time1))
 
 print(datetime.strptime("12/2/18 20:59", '%d/%m/%y %H:%M'))
@@ -31,3 +34,16 @@ print(math.trunc(7.54))
 print(math.ceil(7.54))
 print(math.fsum(number1))
 print(math.fsum(number2))
+
+print(os.getcwd())
+# 执行commond命令
+# os.system('xdg-open .')
+
+print(sys.path)
+
+time1 = time.time()
+print('time1:%f' % (time1))
+time.sleep(3)
+time2 = time.time()
+print('time2:%f' % (time2))
+print('duration:%f' % (time2 - time1))
